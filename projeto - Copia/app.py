4c1f9f30c90b11d9bcf0f2a6166c51e081082e95
guiscg22 <guiscg@gmail.com>
2024-06-24 18:59:55 -0300

    Update app.py

--- projeto - Copia/app.py
+++ projeto - Copia/app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'supersecretkey'
 
