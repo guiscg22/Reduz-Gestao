8b92e10e8176f764ac4c6e74e960c59a4aa1a662
guiscg22 <guiscg@gmail.com>
2024-06-25 20:51:29 -0300

    Update app.py

--- projeto - Copia/app.py
+++ projeto - Copia/app.py
@@ -255,16 +255,21 @@ def compras():
                     compra = Compra(cliente_id=cliente.id)
                     db.session.add(compra)
                 
-                compra.orcamento = request.form.get(f'orcamento_{cliente.id}')
-                compra.nota_fiscal = request.form.get(f'nota_fiscal_{cliente.id}')
-                compra.status = request.form.get(f'status_{cliente.id}')
+                compra.data_compra = datetime.strptime(request.form.get(f'data_compra_{cliente.id}'), '%Y-%m-%d').date() if request.form.get(f'data_compra_{cliente.id}') else None
+                compra.produto = request.form.get(f'produto_{cliente.id}')
+                compra.quantidade = int(request.form.get(f'quantidade_{cliente.id}'))
+                compra.valor_total = float(request.form.get(f'valor_total_{cliente.id}'))
+                compra.status_entrega = request.form.get(f'status_entrega_{cliente.id}')
+                
             db.session.commit()
+            flash('Compras salvas com sucesso!', 'success')
             return redirect(url_for('compras'))
         except Exception as e:
-            flash(f"Erro ao salvar compras: {e}")
-    compras = Compra.query.all()
+            db.session.rollback()
+            flash(f"Erro ao salvar compras: {e}", 'danger')
     clientes = Cliente.query.all()
-    return render_template('compras.html', compras=compras, clientes=clientes)
+    return render_template('compras.html', clientes=clientes)
+
 
 @app.route('/obras', methods=['GET', 'POST'])
 def obras():
