9d5c6f67b7a21fb9b9d3fc0b682b6e608fc01c99
guiscg22 <guiscg@gmail.com>
2024-06-25 00:42:14 -0300

    Update app.py

--- projeto - Copia/app.py
+++ projeto - Copia/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.dialects.postgresql import VARCHAR
 
 app = Flask(__name__)
 
