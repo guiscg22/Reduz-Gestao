ef356c67dc14e6232960684ad2d027c7269650c2
guiscg22 <guiscg@gmail.com>
2024-06-25 00:24:25 -0300

    Update config.py

--- projeto - Copia/config.py
+++ projeto - Copia/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://reduzgestao_user:hAvAVESGXiKKUJfMjddrPWYoOo5oxftE@dpg-cpsvf0mehbks73eroe40-a.oregon-postgres.render.com/reduzgestao')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
